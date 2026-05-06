3fd26ec656cee450b78d6fe4e169abe196d3f06c
doge <doge@gmail.com>
2026-05-06 22:05:17 +0800

    alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { resolve } from "node:path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src")
+    }
+  },
   build: {
     manifest: "manifest.json",
     outDir: "dist",
